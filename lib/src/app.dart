4852eb6ab0c0685cc00db66230bbbb1fffb6eb35
guilherme <guilherme@justdigital.com.br>
2019-05-13 15:00:03 -0300

    User local auth

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,26 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
+// Screens
 import 'package:lol/src/screens/login_screen.dart';
 import 'package:lol/src/screens/user_profile_screen.dart';
+
+// Controllers
 import 'package:lol/src/controllers/user_controller.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _AppState();
+  }
+}
+
+class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<UserAuth>(
       builder: (_) => UserAuth(),
-      child:       MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          brightness: Brightness.dark,
-          accentColor: Colors.cyan[600],
-          buttonColor: Colors.cyan[600],
-        ),
-        home: LoginScreen(),
-        routes: {
-          '/profile':  (BuildContext context) => UserProfileScreen(),
-        },
+      child:  RunMaterialApp(),
+    );
+  }
+}
+
+class RunMaterialApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _RunMaterialAppState();
+  }
+}
+
+class _RunMaterialAppState extends State<RunMaterialApp> {
+  @override
+  Widget build(BuildContext context) {
+    final userAuth = Provider.of<UserAuth>(context);
+
+    if(userAuth.isLogged == null) {
+      userAuth.verifyLogged();
+    }
+
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        accentColor: Colors.cyan[600],
+        buttonColor: Colors.cyan[600],
       ),
+      home: userAuth.isLogged != null ? UserProfileScreen() : LoginScreen(),
+      routes: {
+        '/profile':  (BuildContext context) => UserProfileScreen(),
+      },
     );
   }
 }
--- lib/src/controllers/user_controller.dart
+++ lib/src/controllers/user_controller.dart
@@ -2,22 +2,53 @@ import 'dart:convert';
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:lol/src/models/user_model.dart';
 import 'package:lol/src/controllers/api.dart';
 
 class UserController with ChangeNotifier {
   UserModel _authUser;
+  bool _isLogged;
 }
 
 class User extends UserController {
   UserModel get getUser {
     return _authUser;
   }
+
+  bool get isLogged {
+    return _isLogged;
+  }
 }
 
 class UserAuth extends User {
+
+  Future<bool> verifyLogged() async {
+    // Get local User data
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    if(prefs.getString('name') != null && prefs.getString('name').isNotEmpty) {
+      _authUser = UserModel(
+        profileIconId: prefs.getInt('profileIconId'),
+        name: prefs.getString('name'),
+        puuid: prefs.getString('puuid'),
+        summonerLevel: prefs.getInt('summonerLevel'),
+        accountId: prefs.getString('accountId'),
+        id: prefs.getString('id'),
+        revisionDate: prefs.getInt('revisionDate'),
+      );
+      print('----------------aquiii---------');
+      _isLogged = true;
+      notifyListeners();
+      return true;
+    }
+    return false;
+  }
+
   Future<Map<String, dynamic>> auth(String userName) async {
+    bool success = false;
+    String message = 'Invalid Username';
     http.Response response;
+
     response = await http.get(
       'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/$userName',
       headers: {
@@ -27,13 +58,9 @@ class UserAuth extends User {
     );
 
     final Map<String, dynamic> responseData = json.decode(response.body);
-    print(responseData);
-
-    bool hasError = true;
-    String message = 'Invalid Username';
 
     if(responseData.containsKey('accountId')) {
-      hasError = false;
+      success = true;
       message = 'Authenticated successfully';
 
       _authUser = UserModel(
@@ -45,9 +72,19 @@ class UserAuth extends User {
         id: responseData['id'],
         revisionDate: responseData['revisionDate'],
       );
+
+      // Set local User data
+      final SharedPreferences prefs = await SharedPreferences.getInstance();
+      prefs.setInt('profileIconId', _authUser.profileIconId);
+      prefs.setString('name', _authUser.name);
+      prefs.setString('puuid', _authUser.puuid);
+      prefs.setInt('summonerLevel', _authUser.summonerLevel);
+      prefs.setString('accountId', _authUser.accountId);
+      prefs.setString('id', _authUser.id);
+      prefs.setInt('revisionDate', _authUser.revisionDate);
       notifyListeners();
     }
 
-    return {'success': !hasError, 'message': message};
+    return {'success': success, 'message': message};
   }
 }
