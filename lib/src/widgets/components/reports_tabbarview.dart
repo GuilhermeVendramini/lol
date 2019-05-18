cf0e4beda0aabbfbb57e07d9299d75057affc109
guilherme <guilherme@justdigital.com.br>
2019-05-18 11:11:45 -0300

    Report page

--- lib/src/widgets/components/reports_tabbarview.dart
+++ lib/src/widgets/components/reports_tabbarview.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:flutter_circular_chart/flutter_circular_chart.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:lol/src/controllers/user_matches_controller.dart';
+
+List<dynamic> _playedAt = [];
 
 class ReportsTabBarView extends StatelessWidget {
 
@@ -9,6 +13,18 @@ class ReportsTabBarView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final userMatches = Provider.of<UserMatchesService>(context);
+    String totalGames = '';
+    String mostPlayedAt = '';
+    String lastMatch = '';
+
+    if(userMatches.isMatchesLoaded == true) {
+      totalGames = '${userMatches.getUserMatches.totalGames}';
+      mostPlayedAt = userMatches.mostPlayedAt['lane'];
+      lastMatch = userMatches.lastMatch;
+      _playedAt = userMatches.playedAt;
+    }
+
     final double deviceWidth = MediaQuery.of(context).size.width;
     final double targetWidth = deviceWidth > 650.0 ? 600.0 : deviceWidth * 0.95;
     return Center(
@@ -29,46 +45,6 @@ class ReportsTabBarView extends StatelessWidget {
               ),
               SizedBox(
                 height: 40.0,
-              ),
-              Text(
-                'Lanes',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18.0,
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.all(10.0),
-                height: 180.0,
-                child: Card(
-                  child: Container(
-                    padding: EdgeInsets.all(20.0),
-                    child: ChartLanes(),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 60.0,
-              ),
-              Text(
-                'Roles',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18.0,
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.all(10.0),
-                height: 180.0,
-                child: Card(
-                  child: Container(
-                    padding: EdgeInsets.all(20.0),
-                    child: ChartRoles(),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 60.0,
               ),
               Text(
                 'Results',
@@ -167,6 +143,81 @@ class ReportsTabBarView extends StatelessWidget {
                 ],
               ),
               SizedBox(
+                height: 60.0,
+              ),
+              Text(
+                'Lanes',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18.0,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                height: 220.0,
+                child: Card(
+                  child: Container(
+                    padding: EdgeInsets.all(20.0),
+                    child: ChartLanes(),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 60.0,
+              ),
+              Text(
+                'Resume',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18.0,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                child: Card(
+                  child: Container(
+                    padding: EdgeInsets.all(20.0),
+                    child: Column(
+                      children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            Text(
+                              'Total games:',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(totalGames),
+                          ],
+                        ),
+                        Row(
+                          children: <Widget>[
+                            Text(
+                              'Most played at:',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(mostPlayedAt),
+                          ],
+                        ),
+                        Row(
+                          children: <Widget>[
+                            Text(
+                              'Last match:',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(lastMatch),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
                 height: 40.0,
               ),
             ],
@@ -194,18 +245,19 @@ class ChartLanes extends StatelessWidget {
       seriesList,
       animate: animate,
       vertical: false,
+      barRendererDecorator: charts.BarLabelDecorator(outsideLabelStyleSpec: charts.TextStyleSpec(color: colorTextChart)),
+
 
       domainAxis: charts.OrdinalAxisSpec(
         renderSpec: charts.SmallTickRendererSpec(
-
           // Tick and Label styling here.
           labelStyle: charts.TextStyleSpec(
-            fontSize: 16, // size in Pts.
+            fontSize: 14, // size in Pts.
             color: charts.Color.fromOther(
               color: colorTextChart,
             ),
           ),
-
+          labelAnchor: charts.TickLabelAnchor.centered,
           // Change the line colors to match text color.
           lineStyle:
           charts.LineStyleSpec(color: charts.MaterialPalette.transparent),
@@ -215,13 +267,11 @@ class ChartLanes extends StatelessWidget {
       /// Assign a custom style for the measure axis.
       primaryMeasureAxis: charts.NumericAxisSpec(
         renderSpec: charts.GridlineRendererSpec(
-
           // Tick and Label styling here.
           labelStyle: charts.TextStyleSpec(
             fontSize: 12, // size in Pts.
             color: colorTextChart,
           ),
-
           // Change the line colors to match text color.
           //lineStyle:
           //    charts.LineStyleSpec(color: charts.MaterialPalette.white)
@@ -232,19 +282,21 @@ class ChartLanes extends StatelessWidget {
 
   /// Create series list with single series
   static List<charts.Series<OrdinalLanes, String>> _createSampleData() {
-    final globalLanesData = [
-      OrdinalLanes('Top', 10),
-      OrdinalLanes('Mid', 30),
-      OrdinalLanes('Bottom', 75),
-    ];
 
+    List<OrdinalLanes> globalLanesData = [];
+    
+    _playedAt.forEach((lanes){
+      globalLanesData.add(OrdinalLanes(lanes['lane'], lanes['count']));
+    });
+    
     return [
       charts.Series<OrdinalLanes, String>(
         id: 'Lane',
-        domainFn: (OrdinalLanes sales, _) => sales.lane,
-        measureFn: (OrdinalLanes sales, _) => sales.games,
+        domainFn: (OrdinalLanes lanes, _) => lanes.lane,
+        measureFn: (OrdinalLanes lanes, _) => lanes.count,
         data: globalLanesData,
-        fillColorFn: (OrdinalLanes sales, _) {
+        labelAccessorFn: (OrdinalLanes lanes, _) => lanes.count.toString(),
+        fillColorFn: (OrdinalLanes lanes, _) {
           final color = charts.MaterialPalette.blue.shadeDefault.lighter;
           return color;
         },
@@ -255,89 +307,7 @@ class ChartLanes extends StatelessWidget {
 
 class OrdinalLanes {
   final String lane;
-  final int games;
+  final int count;
 
-  OrdinalLanes(this.lane, this.games);
-}
-
-// Roles
-class ChartRoles extends StatelessWidget {
-  final List<charts.Series> seriesList = _createSampleData();
-  final bool animate = true;
-
-  @override
-  Widget build(BuildContext context) {
-    charts.Color colorTextChart = charts.Color.black;
-
-    if(Theme.of(context).brightness == Brightness.dark) {
-      colorTextChart = charts.Color.white;
-    }
-
-    return new charts.BarChart(
-      seriesList,
-      animate: animate,
-      vertical: false,
-
-      domainAxis: charts.OrdinalAxisSpec(
-        renderSpec: charts.SmallTickRendererSpec(
-
-          // Tick and Label styling here.
-          labelStyle: charts.TextStyleSpec(
-            fontSize: 16, // size in Pts.
-            color: colorTextChart,
-          ),
-
-          // Change the line colors to match text color.
-          lineStyle:
-          charts.LineStyleSpec(color: charts.MaterialPalette.transparent),
-        ),
-      ),
-
-      /// Assign a custom style for the measure axis.
-      primaryMeasureAxis: charts.NumericAxisSpec(
-        renderSpec: charts.GridlineRendererSpec(
-
-          // Tick and Label styling here.
-          labelStyle: charts.TextStyleSpec(
-            fontSize: 12, // size in Pts.
-            color: colorTextChart,
-          ),
-
-          // Change the line colors to match text color.
-          //lineStyle:
-          //    charts.LineStyleSpec(color: charts.MaterialPalette.white)
-        ),
-      ),
-    );
-  }
-
-  /// Create series list with single series
-  static List<charts.Series<OrdinalRoles, String>> _createSampleData() {
-    final globalRolesData = [
-      OrdinalRoles('Solo', 10),
-      OrdinalRoles('Support', 30),
-      OrdinalRoles('Carry', 75),
-      OrdinalRoles('Jungle', 4),
-    ];
-
-    return [
-      charts.Series<OrdinalRoles, String>(
-        id: 'Lane',
-        domainFn: (OrdinalRoles roles, _) => roles.role,
-        measureFn: (OrdinalRoles roles, _) => roles.games,
-        data: globalRolesData,
-        fillColorFn: (OrdinalRoles roles, _) {
-          final color = charts.MaterialPalette.yellow.shadeDefault.lighter;
-          return color;
-        },
-      ),
-    ];
-  }
-}
-
-class OrdinalRoles {
-  final String role;
-  final int games;
-
-  OrdinalRoles(this.role, this.games);
+  OrdinalLanes(this.lane, this.count);
 }
